cce5156f12166d672f404d5f588f908076149a33
Lucas Dela Justina <lucashdj13@gmail.com>
2023-10-03 19:52:41 +0000

    fatos vendas atualizada com novas dimensoes

--- models/marts/fct_vendas.sql
+++ models/marts/fct_vendas.sql
@@ -14,6 +14,17 @@ with
         from {{ ref('int_vendas_pedido_itens') }}
     )  
 
+        , dim_funcionarios as (
+            select *
+            from {{ ref('dim_funcionarios') }}
+    )
+
+         , dim_fornecedores as (
+            select *
+            from {{ ref('dim_fornecedores') }}
+    )
+
+
          , join_tabelas as (
         select
             pedidos.sk_pedido_item
@@ -40,12 +51,52 @@ with
             , dim_produtos.nome_fornecedor
             , dim_produtos.is_discontinuado
             , dim_clientes.nome_cliente
+            , dim_funcionarios.pk_funcionario
+            , dim_funcionarios.ultimo_nome_funcionario
+            , dim_funcionarios.primeiro_nome_funcionario
+            , dim_funcionarios.cargo_funcionario
+            , dim_funcionarios.pronome_funcionario
+            , dim_funcionarios.data_nascimento_funcionario
+            , dim_funcionarios.data_admissao_funcionario
+            , dim_funcionarios.endereco_funcionario
+            , dim_funcionarios.cidade_funcionario
+            , dim_funcionarios.regiao_funcionario
+            , dim_funcionarios.cep_funcionario
+            , dim_funcionarios.pais_funcionario
+            , dim_funcionarios.telefone_funcionario
+            , dim_funcionarios.extencao_funcionario
+            , dim_funcionarios.relatorios_funcionario
+            , dim_funcionarios.descricao_regiao
+            , dim_funcionarios.id_territorio
+            , dim_funcionarios.descricao_territorio
+            , dim_funcionarios.id_regiao
+            , dim_fornecedores.pk_fornecedor
+            , dim_fornecedores.id_produto
+            , dim_fornecedores.id_fornecedor
+            , dim_fornecedores.id_categoria
+            , dim_fornecedores.quantidade_por_unidade
+            , dim_fornecedores.preco_por_unidade
+            , dim_fornecedores.unidades_em_estoque
+            , dim_fornecedores.unidades_por_ordem
+            , dim_fornecedores.nivel_reabastecimento
+            , dim_fornecedores.contato_fornecedor
+            , dim_fornecedores.contato_funcao
+            , dim_fornecedores.endereco_fornecedor
+            , dim_fornecedores.cidade_fornecedor
+            , dim_fornecedores.regiao_fornecedor
+            , dim_fornecedores.pais_fornecedor
+            , dim_fornecedores.cep_fornecedor
         from pedido_itens as pedidos
         left join dim_produtos on
             pedidos.id_produto = dim_produtos.id_produto
         left join dim_clientes on
             pedidos.id_cliente = dim_clientes.id_cliente
-    )   
+       left join dim_funcionarios on  
+            pedidos.id_funcionario = dim_funcionarios.id_funcionario
+        left join dim_fornecedores on 
+            dim_fornecedores.id_fornecedor = dim_produtos.id_fornecedor
+
+    )
     
          , transformacoes as (
             select
--- models/staging/erp-northwind/stg_erp_regioes.sql
+++ models/staging/erp-northwind/stg_erp_regioes.sql
@@ -1,6 +1,6 @@
 with
     fonte_regioes as (
-        select cast(region_id as string) as id_regiao
+        select cast(region_id as int) as id_regiao
             , cast(region_description as string) as descricao_regiao
         from {{ ref('region') }}
     )
